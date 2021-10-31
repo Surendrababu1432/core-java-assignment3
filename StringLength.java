29d173991069d0202fa60cdadcca0ba0a3300dd6
Surendrababu1432 <92866299+Surendrababu1432@users.noreply.github.com>
2021-10-31 09:44:11 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class StringLength {
+	
+	public static void main(String[] args) {
+		
+		String str = "my name is surendrababu";
+		int length = str.length();
+		System.out.println("The length of the given string is " +length);
+	}
+	
+}
---
+++ Stringconcat.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Stringconcat {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Hello";
+		String str2 = " I'm surendrababu";
+		String str3 = str1.concat(str2);
+		System.out.println(str3);
+	}
+
+}
